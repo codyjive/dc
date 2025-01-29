eebe233c7ab01cdd59a01bb37328907a9f2c5b4e
codyjive <163155566+codyjive@users.noreply.github.com>
2025-01-29 09:59:20 -0700

    Update SpecialOffers.js

--- src/components/SpecialOffers.js
+++ src/components/SpecialOffers.js
@@ -3,14 +3,17 @@ import VehicleCard from './VehicleCard';
 
 const SpecialOffers = ({ vehicles }) => {
   return (
-    <div className="special-offers">
-      {vehicles.length > 0 ? (
-        vehicles.map((vehicle) => (
-          <VehicleCard key={vehicle.vin} vehicle={vehicle} />
-        ))
-      ) : (
-        <p className="no-offers">No special offers available at this time.</p>
-      )}
+    <div className="container">
+      <h1 className="header-title">New Honda Lease Specials</h1>
+      <div className="special-offers">
+        {vehicles.length > 0 ? (
+          vehicles.map((vehicle) => (
+            <VehicleCard key={vehicle.vin} vehicle={vehicle} />
+          ))
+        ) : (
+          <p className="no-offers">No special offers available at this time.</p>
+        )}
+      </div>
     </div>
   );
 };
